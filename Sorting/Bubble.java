ec6819862cd09dacb5122e864801a7bc76fe5684
anuragtiwari3100 <anuragtiwarigpj2005@gmail.con>
2024-11-09 23:31:50 +0530

    No changes made

--- Sorting/Bubble.java
+++ Sorting/Bubble.java
@@ -1,17 +1,25 @@
 import java.util.Scanner;
 public class Bubble{
     public static void bubbleSort(int arr[]){
-        for(int turn=0;turn<arr.length;turn++)   {
+        int n =arr.length;
+        for(int turn=0;turn<n-1;turn++)   {
             //arr.length-1 because we do not worry about the the last   element b/c last element will automqatically conarin the maximum among all   values
-            for(int j=0;j<arr.length-1-turn;j++){
-                if(arr[j]>arr[j+1]){
+            for(int j=0;j<n-1-turn;j++){
+                if(arr[j]<arr[j+1]){
                     //swap
                     int temp=arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
+                    // Swap(arr,j, j+1);    we can use this method also,   passing the   indices instead  of values
                 }
             }
         }
+    }
+
+    public static void Swap(int arr[], int i, int j){
+         int temp = arr[i];
+         arr[i] = arr[j];
+         arr[j] = temp;
     }
          public static void print(int arr[]){
                for(int i=0;i<arr.length;i++){
