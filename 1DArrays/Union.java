86fe62db0a4ff2d221580d064a5892f1a8746850
anuragtiwari3100 <anuragtiwarigpj2005@gmail.con>
2025-01-29 23:02:25 +0530

    added the optimal solution

--- 1DArrays/Union.java
+++ 1DArrays/Union.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 class  Union {
-            //Approach1
+    
+            //Approach1 => Not Suitable for some TCs
             public static  ArrayList<Integer> findUnion(int arr1[], int arr2[]){
                 int n= arr1.length;
                 int m = arr2.length;
@@ -19,6 +21,20 @@ class  Union {
             }
             return Union;
             }
+
+            //Approach2
+                        static ArrayList<Integer> FindUnion(int arr1[], int arr2[], int n, int m) {
+            HashSet <Integer> s=new HashSet<>();
+            ArrayList < Integer > Union=new ArrayList<>();
+            for (int i = 0; i < n; i++)
+                s.add(arr1[i]);
+            for (int i = 0; i < m; i++)
+                s.add(arr2[i]);
+            for (int it: s)
+                Union.add(it);
+            return Union;
+            }
+
     public static void main(String[] args) {
   int arr1[] = {1,2,3,4,5,9,10};
   int arr2[] = {3,4,5,6,7,8};
--- 1DArrays/moveZerosToLeft.java
+++ 1DArrays/moveZerosToLeft.java
@@ -20,29 +20,8 @@ public class moveZerosToLeft {
                 System.out.print(arr[i]);
             }
         }
-
-        public static void movefirst(int arr[]) {
-            int index = arr.length - 1; // Start from the end of the array
-        
-            // Step 1: Place all non-zero elements at the end
-            for (int i = arr.length - 1; i >= 0; i--) {
-                if (arr[i] != 0) {
-                    arr[index--] = arr[i];
-                }
-            }
-        
-            // Step 2: Fill the remaining positions at the start with zeros
-            while (index >= 0) {
-                arr[index--] = 0;
-            }
-        
-            // Optional: Print the updated array for verification
-            System.out.println("Updated array: " + Arrays.toString(arr));
-        }
-        
     public static void main(String args[]){
         int arr[] = new int[]{1,0,5,3,0,2,0};
         moveZerosLeft(arr);
-    }
-    
+    }   
 }
